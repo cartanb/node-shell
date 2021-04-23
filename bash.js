55c6c54bb239ef20dd23579082f50f25dbab3037
Phillip Kim <Phillip.Kim.3112@gmail.com>
2021-04-23 12:02:07 -0500

    put things in seperate files. added LS command

--- bash.js
+++ bash.js
@@ -1,13 +1,17 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
 //output a prompt
-process.stdout.write("prompt > ");
+process.stdout.write('prompt > ');
 
 //stdin 'data' event fires after user types a line
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if (cmd === "pwd") {
-    process.stdout.write(process.env.PWD);
+  if (cmd === 'pwd') {
+    pwd();
+  } else if (cmd === 'ls') {
+    ls();
   } else {
-    process.stdout.write("You typed: " + cmd);
+    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('\nprompt > ');
   }
-  process.stdout.write("\nprompt > ");
 });
